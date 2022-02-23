2acb80f600669277e0dae063736ba2cd0dc2ac14
Eron9528 <1551447510@qq.com>
2022-02-23 23:44:46 +0800

    面试题更新2

---
+++ interviews/src/cn/com/javasehigh/c_designmode_garbagecollect/a_designmode/H_ClassAdapter.java
@@ -0,0 +1,30 @@
+package cn.com.javasehigh.c_designmode_garbagecollect.a_designmode;
+
+public class H_ClassAdapter {
+    public static void main(String[] args) {
+        Adapter adapter = new Adapter();
+        adapter.method1();
+        adapter.method2();
+    }
+}
+
+class Source{
+    public void method1(){
+        System.out.println(" this is original method!");
+    }
+}
+
+interface Targetable{
+    // 与原类的方法相同
+    public void method1();
+    // 新类的方法
+    public void method2();
+}
+
+class Adapter extends Source implements Targetable{
+
+    @Override
+    public void method2() {
+        System.out.println("this is the targetable method!");
+    }
+}
---
+++ interviews/src/cn/com/javasehigh/c_designmode_garbagecollect/a_designmode/K_Strategy.java
@@ -0,0 +1,44 @@
+package cn.com.javasehigh.c_designmode_garbagecollect.a_designmode;
+
+import javax.swing.*;
+
+public class K_Strategy {
+    public static void main(String[] args) {
+        String exp = "2+8";
+        ICalculator cal = new Plus();
+        int calculate = cal.calculate(exp);
+        System.out.println(calculate);
+    }
+}
+
+interface ICalculator {
+    public int calculate(String exp);
+}
+
+class AbstractCalculate{
+    public int[] split(String exp, String opt){
+        String array[] = exp.split(opt);
+        int arrayInt[] = new int[2];
+        arrayInt[0] = Integer.parseInt(array[0]);
+        arrayInt[1] = Integer.parseInt(array[1]);
+        return arrayInt;
+    }
+}
+
+class Minus extends AbstractCalculate implements ICalculator{
+
+    @Override
+    public int calculate(String exp) {
+        int arrayInt[] = split(exp, "-");
+        return arrayInt[0]-arrayInt[1];
+    }
+}
+
+class Plus extends AbstractCalculate implements ICalculator{
+
+    @Override
+    public int calculate(String exp) {
+        int arrayInt[] = split(exp, "\\+");
+        return arrayInt[0]+arrayInt[1];
+    }
+}
