c207c94f8a92e48af35148e615b8ebf8e0793887
Eron9528 <1551447510@qq.com>
2022-02-26 23:23:53 +0800

    面试题更新4

--- spring5/src/cn/constructs/principle/dependence_inversion/Main.java
+++
@@ -1,9 +0,0 @@
-package cn.constructs.principle.dependence_inversion;
-
-public class Main {
-    public static void main(String[] args) {
-        Tom tom = new Tom();
-        tom.studyJavaCourse();
-        tom.studyPythonCourse();
-    }
-}
--- spring5/src/cn/constructs/principle/open_close/ICourse.java
+++
@@ -1,7 +0,0 @@
-package cn.constructs.principle.open_close;
-
-public interface ICourse {
-    Integer getId();
-    String getName();
-    Double getPrice();
-}
