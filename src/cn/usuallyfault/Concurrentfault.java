bb5dc1dc8393798414ed61c0ca1ceb70cf3f5b0d
mayul <123456>
2022-02-28 17:40:23 +0800

    面试题11

---
+++ src/cn/usuallyfault/Concurrentfault.java
@@ -0,0 +1,11 @@
+package cn.usuallyfault;
+
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Concurrentfault {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList list = null;
+        ArrayList list1 = null;
+    }
+}
